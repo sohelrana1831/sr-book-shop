310fe83687c381da377e304bcc1b62face5c53fc
sohel rana <sohel.rana@apsissolutions.com>
2023-07-25 00:05:29 +0600

    API update

--- src/redux/api/apiSlices.ts
+++ src/redux/api/apiSlices.ts
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // Define a service using a base URL and expected endpoints
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://sr-book-shop-backend.onrender.com/api/v1',
+  }),
   tagTypes: ['book', 'review'],
   endpoints: () => ({}),
 });
